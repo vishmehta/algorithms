3e2253891f17ff9b4dfdb7b8c4f696a394b10141
vishmehta <vish.s.mehta@gmail.com>
2016-11-14 17:50:39 +0000

    Add blocking wait and notify

---
+++ src/com/vishal/queue/BlockingQueueWaitAndNotifyAll.java
@@ -0,0 +1,35 @@
+package com.vishal.queue;
+
+//http://tutorials.jenkov.com/java-concurrency/blocking-queues.html
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BlockingQueueWaitAndNotifyAll {
+
+    private final int LIMIT = 10;
+    private List queue = new LinkedList();
+
+    public synchronized void enqueue(Object item) throws InterruptedException {
+
+        while (queue.size() == LIMIT) {
+            wait();
+        }
+        if (queue.size() == 0) {
+            notifyAll();
+        }
+        queue.add(item);
+    }
+
+    public synchronized void dequeue(Object item) throws InterruptedException {
+
+        while (queue.size() == 0) {
+            wait();
+        }
+        if (queue.size() == LIMIT) {
+            notifyAll();
+        }
+        queue.remove(item);
+    }
+
+}
